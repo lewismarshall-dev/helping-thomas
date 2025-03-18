4b73787ce40774e35b751f4b15b0f027dac25654
Lewis Marshall <lewis.marshall465@gmail.com>
2025-03-18 21:26:17 +1300

    Performance fix

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 'use strict';
-
 let last = { x: 0, y: 0 };
 let imageList = [];
 let index = 0;
-
-let fadeOutDelay = 1000;
-let transitionDuration = 750;
-document.documentElement.style.setProperty('--transition-duration', `${transitionDuration}ms`);
 
 // Fetch placeholder images
 fetch('https://picsum.photos/v2/list?limit=100')
@@ -21,44 +16,50 @@ fetch('https://picsum.photos/v2/list?limit=100')
   })
   .catch((error) => console.error('Error:', error));
 
-// Observe mouse movement
-document.addEventListener('mousemove', (event) => {
-  // Set current cursor position
-  const current = { x: event.clientX, y: event.clientY };
+document.addEventListener('DOMContentLoaded', () => {
+  let fadeOutDelay = 1000;
+  let transitionDuration = 750;
+  document.documentElement.style.setProperty('--transition-duration', `${transitionDuration}ms`);
 
-  // If the cursor has moved more than 50 pixels
-  if (calcPositionDistance(last, current) > 100) {
-    // Add image from list to the DOM
-    const img = imageList[index].cloneNode();
-    document.body.appendChild(img);
+  // Observe mouse movement
+  document.addEventListener('mousemove', (event) => {
+    // Set current cursor position
+    const current = { x: event.clientX, y: event.clientY };
 
-    img.onload = () => {
-      // Set image center position to cursor, set image visible
-      img.style.left = current.x - img.width / 2 + 'px';
-      img.style.top = current.y - img.height / 2 + 'px';
+    // If the cursor has moved more than 50 pixels
+    if (calcPositionDistance(last, current) > 100) {
+      // Add image from list to the DOM
+      const img = imageList[index].cloneNode();
+      document.body.appendChild(img);
 
-      // Transition in: scale from 0.5 (set in css) to 1
-      requestAnimationFrame(() => {
-        img.style.visibility = 'visible';
-        img.style.transform = 'scale(1)';
-      });
+      img.onload = () => {
+        // Set image center position to cursor, set image visible
+        img.style.left = current.x - img.width / 2 + 'px';
+        img.style.top = current.y - img.height / 2 + 'px';
 
-      // Transition out after delay: scale to 0.5, fade out
-      setTimeout(() => {
-        img.style.transform = 'scale(0.5)';
-        img.style.opacity = 0;
-      }, fadeOutDelay);
+        // Transition in: scale from 0.5 (set in css) to 1
+        requestAnimationFrame(() => {
+          img.style.visibility = 'visible';
+          img.style.transform = 'scale(1)';
+        });
 
-      // Remove image from DOM after transition
-      setTimeout(() => img.remove(), fadeOutDelay + transitionDuration);
+        // Transition out after delay: scale to 0.5, fade out
+        setTimeout(() => {
+          img.style.transform = 'scale(0.5)';
+          img.style.opacity = 0;
+        }, fadeOutDelay);
 
-      // Set last cursor position to current
-      last = current;
+        // Remove image from DOM after transition
+        setTimeout(() => img.remove(), fadeOutDelay + transitionDuration);
 
-      // Set next image index
-      index = (index + 1) % imageList.length;
-    };
-  }
+        // Set last cursor position to current
+        last = current;
+
+        // Set next image index
+        index = (index + 1) % imageList.length;
+      };
+    }
+  });
 });
 
 function calcPositionDistance(last, current) {
